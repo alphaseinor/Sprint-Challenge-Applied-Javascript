dc585817c2d69bddfad4e37229c901d47b214cb0
Brian Hague <alphaseinor@gmail.com>
2019-09-13 21:58:57 -0500

    adding easier carousel in js only... only needs one image at a time.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -18,37 +18,54 @@
   </div>
 */
 
+images = [['./assets/carousel/mountains.jpeg','mountains'],
+['./assets/carousel/computer.jpeg','computer'],
+['./assets/carousel/trees.jpeg','trees'],
+['./assets/carousel/turntable.jpeg','turntable']]
+
 addCarousel = () => {
+  carouselIndex = 0;
   carousel = document.createElement('div')
   left = document.createElement('div')
-  img1 = document.createElement('img')
-  img2 = document.createElement('img')
-  img3 = document.createElement('img')
-  img4 = document.createElement('img')
+  img = document.createElement('img')
+  //img2 = document.createElement('img')
+  //img3 = document.createElement('img')
+  //img4 = document.createElement('img')
   right = document.createElement('div')
 
   carousel.appendChild(left)
-  carousel.appendChild(img1)
-  carousel.appendChild(img2)
-  carousel.appendChild(img3)
-  carousel.appendChild(img4)
+  carousel.appendChild(img)
+  //carousel.appendChild(img2)
+  //carousel.appendChild(img3)
+  //carousel.appendChild(img4)
   carousel.appendChild(right)
 
-  img1.setAttribute('src', './assets/carousel/mountains.jpeg')
-  img2.setAttribute('src', './assets/carousel/computer.jpeg')
-  img3.setAttribute('src', './assets/carousel/trees.jpeg')
-  img4.setAttribute('src', './assets/carousel/turntable.jpeg')
+  img.setAttribute('src', images[carouselIndex][0])
+  //img2.setAttribute('src', './assets/carousel/computer.jpeg')
+  //img3.setAttribute('src', './assets/carousel/trees.jpeg')
+  //img4.setAttribute('src', './assets/carousel/turntable.jpeg')
 
-  img1.setAttribute('alt', 'mountains')
-  img2.setAttribute('alt', 'computer')
-  img3.setAttribute('alt', 'trees')
-  img4.setAttribute('alt', 'turntable')
+  img.setAttribute('alt', images[carouselIndex][1])
+  //img2.setAttribute('alt', 'computer')
+  //img3.setAttribute('alt', 'trees')
+  //img4.setAttribute('alt', 'turntable')
 
-  img1.classList.add('show')
+  img.classList.add('show')
 
   carousel.classList.add('carousel')
   left.classList.add('left-button')
   right.classList.add('right-button')
+
+  left.addEventListener('click', x => {
+    //go back one, or go to the top
+    console.log(images[0][0])
+    //img.setAttribute('src', images[0][1])
+  })
+  right.addEventListener('click', x => {
+    console.log(images[1][0])
+    //go forward one, or go to the beginning
+    //img.setAttribute('src', images[0][0])
+  })
 
   return carousel
 }
